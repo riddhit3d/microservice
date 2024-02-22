36af31274649c698672de032190d120daa3f199c
riddhit3d <112148058+riddhit3d@users.noreply.github.com>
2024-02-22 17:53:29 +0530

    Add files via upload

--- micro_rid/QuizService1/src/main/java/com/quiz/QuizService1/QuizService1Application.java
+++ micro_rid/QuizService1/src/main/java/com/quiz/QuizService1/QuizService1Application.java
@@ -2,9 +2,12 @@ package com.quiz.QuizService1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+
 @EnableFeignClients
 public class QuizService1Application {
 
